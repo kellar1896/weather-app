6d824356e76cf7912b2ecb1885c7664d16357d38
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-19 17:39:00 +0300

    add cities slice to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import citiesReducer from '../features/cities/citiesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    cities: citiesReducer,
   },
 });
 
