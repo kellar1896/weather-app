09708c89a97c585d80cf974a45f8ef46771e0c65
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-20 11:26:00 +0300

    format code

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,11 +1,9 @@
 export class HttpClient {
-    async get(path: string) {
-      const response = await fetch(path);
-      if (!response.ok) {
-        throw new Error(`HTTP ${response.status} - ${response.statusText}`);
-      }
-      return response.json();
+  async get(path: string) {
+    const response = await fetch(path);
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status} - ${response.statusText}`);
     }
-  
+    return response.json();
+  }
 }
-  
