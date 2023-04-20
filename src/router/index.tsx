eb78983e95bd94e2e4b57fb6146ab6cf80a0ae13
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-20 14:30:10 +0300

    create router handler component

---
+++ src/router/index.tsx
@@ -0,0 +1,26 @@
+import React, { useCallback } from "react";
+import { Route, Routes } from "react-router-dom";
+import { PageRoute } from "../app/types";
+import { routes } from "./routes.config";
+
+
+const Router = () => {
+
+    const renderRoute = useCallback((route: PageRoute) => {
+        return (
+            <Route path={route.path} element={<route.element/>}>
+                {route.children?.map((childRoute) => renderRoute(childRoute))}
+            </Route>
+        )
+    }, [])
+
+    return (
+        <Routes>
+          {routes.map((route)=>
+              renderRoute(route)
+          )}
+        </Routes>
+    );
+}
+
+export default Router
