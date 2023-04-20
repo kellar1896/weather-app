a34ceae77307c1c445d474b24daa56b67fb637c0
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-20 11:25:06 +0300

    wip app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { loadCities } from "./features/cities/citiesSlice";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import LoadingIntro from "./components/molecules/loading-intro";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const cities = useAppSelector((state) => state.cities.cities);
+  const isLoadingCities = useAppSelector((state) => state.cities.isLoading)
+
+  useEffect(() => {
+    dispatch(loadCities());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      
+      {isLoadingCities ? <LoadingIntro /> : null}
+      {cities.slice(0,100).map((city, index) => 
+        <span key={`city_${index}`}>{city.name}</span>
+      )}
     </div>
   );
 }
