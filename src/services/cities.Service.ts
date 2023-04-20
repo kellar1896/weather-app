35ae2613758421630e2be20493b15a8449ddeb7b
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-20 10:47:24 +0300

    create service to fetch global cities

--- src/services/cities.Service.ts
+++ src/services/cities.Service.ts
@@ -1,19 +1,17 @@
-import { City } from "../app/types";
 import { HttpClient } from "./httpClient";
 
-const apiKey = process.env.REACT_APP_CITIES_API_KEY
 
 export class CitiesServices {
+    apiKey = process.env.REACT_APP_CITIES_API_KEY;
     httpClient = new HttpClient();
   
     fetchCities = async () => {
       try {
-        const baseUrl = `https://airlabs.co/api/v9/cities?&_fields=name,city_code,lat,lng&api_key=${apiKey}`;
+        const baseUrl = `https://airlabs.co/api/v9/cities?&_fields=name,city_code,lat,lng&api_key=${this.apiKey}`;
         const response = await this.httpClient.get(baseUrl);
         return response;
       } catch (error) {
-        console.log(`Error ${error}`);
-        throw new Error("Unable to fetch user data");
+        throw new Error(`Unable to fetch user data error msg: ${error}`);
       }
     };
   }
