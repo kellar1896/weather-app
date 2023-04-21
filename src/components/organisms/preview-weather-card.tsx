d57106bf41d5ac69294b18df2d617fbe734e2a82
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-21 14:32:43 +0300

    create preview city weather card component

---
+++ src/components/organisms/preview-weather-card.tsx
@@ -0,0 +1,67 @@
+import React, { memo, useCallback } from "react";
+import { CityWeather } from "../../app/types/weather";
+import iconWeather from "../../assets/icons/day/01d.png";
+import { getIconImage } from "../../tools/iconsProvider";
+
+type PreviewWeatherCardProps = {
+  cityWeather: CityWeather;
+  onClick?: () => void;
+};
+
+
+const PreviewWeatherCard = ({
+  cityWeather,
+  onClick,
+}: PreviewWeatherCardProps) => {
+  const renderTimeZone = useCallback(() => {
+    if (cityWeather?.isLoading) return "---";
+    return cityWeather?.timezone
+  }, [cityWeather]);
+
+  const renderTemp = useCallback(()=>{
+    if (cityWeather?.isLoading) return "---";
+    return  `${cityWeather?.current.temp} °C`
+  },[cityWeather])
+
+  const renderFeelsLike = useCallback(()=>{
+    if (cityWeather?.isLoading) return "---";
+    return `${cityWeather?.current.feels_like} °C`
+  },[cityWeather])
+
+  const renderHumidity = useCallback(()=>{
+    if (cityWeather?.isLoading) return "---";
+    return `${cityWeather?.current.humidity}%`
+  },[cityWeather])
+
+  const renderIconImage = useCallback(()=>{
+    if(cityWeather.isLoading) return iconWeather
+    // return `http://openweathermap.org/img/wn/${cityWeather?.current.weather[0].icon}@2x.png`
+    return getIconImage(cityWeather.current.weather[0].icon)
+  },[cityWeather])
+
+console.log(cityWeather)
+  return (
+    <div className="bg-ivory rounded-xl p-2 text-jet w-52" onClick={onClick}>
+      <div className="flex flex-col items-start">
+        <h4 className="font-bold text-lg">{cityWeather?.city.name}</h4>
+        <span className="text-xs">
+          Mon, 1:00 pm, Time Zone: {renderTimeZone()}
+        </span>
+      </div>
+      <div>
+        <div className="flex items-center justify-around">
+          <img src={renderIconImage()} alt="current_weather" className="w-16" />
+          <span className="font-extrabold text-xl md:text-2xl">
+            {renderTemp()}
+          </span>
+        </div>
+        <div className="flex justify-between text-xs">
+          <span>Feels like {renderFeelsLike()}</span>
+          <span>Humidity {renderHumidity()}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(PreviewWeatherCard);
