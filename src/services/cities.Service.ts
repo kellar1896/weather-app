a73be63765fce4784504cedd7f4b9472f52df196
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-21 10:40:59 +0300

    read api url from env

--- src/services/cities.Service.ts
+++ src/services/cities.Service.ts
@@ -3,11 +3,12 @@ import { HttpClient } from "./httpClient";
 
 export class CitiesServices {
     apiKey = process.env.REACT_APP_CITIES_API_KEY;
+    apiUrl = process.env.REACT_APP_CITIES_API_URL;
     httpClient = new HttpClient();
   
     fetchCities = async () => {
       try {
-        const baseUrl = `https://airlabs.co/api/v9/cities?&_fields=name,city_code,lat,lng&api_key=${this.apiKey}`;
+        const baseUrl = `${this.apiUrl}?&_fields=name,city_code,lat,lng&api_key=${this.apiKey}`;
         const response = await this.httpClient.get(baseUrl);
         return response;
       } catch (error) {
