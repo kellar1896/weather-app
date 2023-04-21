58758de074ec79479e3f6bd8d8e51ef1510c4be6
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-21 17:05:28 +0300

    wip city forecast screen

--- src/pages/city.tsx
+++ src/pages/city.tsx
@@ -1,12 +1,38 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../app/hooks";
+import { selectCityWeatherByCityCode } from "../features/weathers/weathersSlice";
+import PreviewWeatherCard from "../components/organisms/preview-weather-card";
+import WeatherForecastCard from "../components/organisms/weather-forecast-card";
 
 const CityPage = () => {
-    return(
-        <div>
-            City PAGE
-        </div>
-    )
-}
+  const { cityId } = useParams();
+  const cityWeather = useAppSelector((state) =>
+    selectCityWeatherByCityCode(state, cityId)
+  );
+  if (cityWeather === undefined)
+    return (
+      <div className="h-screen w-screen bg-jet text-ivory flex justify-center items-center">
+        <p className="text-xl font-bold">sorry couldn't load this URL</p>
+      </div>
+    );
+  return (
+    <div className="h-screen w-screen overflow-auto bg-jet">
+      <div className="w-full md:w-10/12 mx-auto">
+        <PreviewWeatherCard cityWeather={cityWeather} />
+      </div>
+      <div className="flex flex-col md:flex-wrap md:flex-row w-full md:w-10/12 mx-auto md:justify-between">
+        {cityWeather.daily.map((dailyWeather, index) => {
+          return (
+            <WeatherForecastCard
+              dailyWeather={dailyWeather}
+              key={`${cityWeather.city.city_code}_${index}`}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default CityPage
+export default CityPage;
