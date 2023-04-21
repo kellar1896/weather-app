a0d3365f96fe1c010f6a8db5f2e134a02026c79c
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-21 17:29:10 +0300

    create list component

---
+++ src/components/molecules/weather-data.tsx
@@ -0,0 +1,41 @@
+import React, { memo } from "react";
+import WeatherFeature from "../atoms/weather-feature";
+import { getAmPmTime } from "../../tools";
+
+type WeatherDataProps = {
+  tempMax?: number;
+  tempMin?: number;
+  sunrise: number;
+  sunset: number;
+  humidity: number;
+  windSpeed: number;
+};
+
+const WeatherData = ({
+  humidity,
+  sunrise,
+  sunset,
+  tempMax,
+  tempMin,
+  windSpeed,
+}: WeatherDataProps) => {
+  return (
+    <div className="flex flex-row justify-around w-full">
+      {tempMax && (
+        <div className="flex flex-col items-start">
+          <span className="text-jet">Temp</span>
+          <WeatherFeature label="Min:" value={`${tempMin}°C`} />
+          <WeatherFeature label="Max:" value={`${tempMax}°C`} />
+        </div>
+      )}
+      <div className="flex flex-col items-start">
+        <WeatherFeature label="Sunrise" value={getAmPmTime(sunrise)} />
+        <WeatherFeature label="Sunset" value={getAmPmTime(sunset)} />
+        <WeatherFeature label="Humidity" value={`${humidity}%`} />
+        <WeatherFeature label="Wind Speed" value={`${windSpeed}m/s`} />
+      </div>
+    </div>
+  );
+};
+
+export default memo(WeatherData);
